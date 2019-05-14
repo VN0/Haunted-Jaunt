63b4d6c724c109cbfba2c921f94f45fdcc65879e
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-05-14 23:02:54 +0530

    Added the game ending behaviour

---
+++ Haunted Jaunt/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,49 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    private bool _isPlayerAtExit;
+    private float _timer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            _isPlayerAtExit = true;
+            Debug.Log("Player spotted");
+        }
+    }
+
+    void Update()
+    {
+        if (_isPlayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel()
+    {
+        _timer += Time.deltaTime;
+
+        exitBackgroundImageCanvasGroup.alpha = _timer / fadeDuration;
+
+        if (_timer > fadeDuration + displayImageDuration)
+        {
+            Debug.Log("Quitting");
+            Application.Quit();
+        }
+    }
+}
