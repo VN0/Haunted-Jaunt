b499d3ec26d80d0434ac54d30af4491a5895008a
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-05-15 09:57:59 +0530

    Positioned the ghosts

---
+++ Haunted Jaunt/Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,30 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    private int _currentWaypointIndex;
+
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[_currentWaypointIndex].position);
+        }
+    }
+}
