35dca7060a2a6f6f36649e07b8c34f27492eb669
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-05-15 10:55:26 +0530

    Set up the ghost sfx

--- Haunted Jaunt/Assets/Scripts/PlayerMovement.cs
+++ Haunted Jaunt/Assets/Scripts/PlayerMovement.cs
@@ -15,11 +15,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _rb;
 
     public float turnSpeed = 20f;
+    public AudioSource _footsteps;
 
     void Start()
     {
         _anim = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody>();
+        _footsteps = GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
@@ -35,6 +37,18 @@ public class PlayerMovement : MonoBehaviour
 
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         _anim.SetBool("IsWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!_footsteps.isPlaying)
+            {
+                _footsteps.Play();
+            }
+        }
+        else
+        {
+            _footsteps.Stop();
+        }
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, _movement, turnSpeed * Time.deltaTime, 0f);
         _rotation = Quaternion.LookRotation(desiredForward);
