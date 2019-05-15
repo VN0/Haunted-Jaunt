1536b699e9a9ad2b409e8d0215bda7326c56b499
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-05-15 10:40:23 +0530

    Fixed level reloading

--- Haunted Jaunt/Assets/Scripts/GameEnding.cs
+++ Haunted Jaunt/Assets/Scripts/GameEnding.cs
@@ -15,10 +15,13 @@ public class GameEnding : MonoBehaviour
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
 
     private bool _isPlayerAtExit;
     private bool _isPlayerCaught;
     private float _timer;
+    private bool _hasAudioPlayed;
 
     void OnTriggerEnter(Collider other)
     {
@@ -38,28 +41,39 @@ public class GameEnding : MonoBehaviour
     {
         if (_isPlayerAtExit)
         {
-            EndLevel(exitBackgroundImageCanvasGroup, false);
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
         }
         else if (_isPlayerCaught)
         {
-            EndLevel(caughtBackgroundImageCanvasGroup, true);
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     }
 
-    void EndLevel(CanvasGroup imageCanvasGroup, bool toRestart)
+    void EndLevel(CanvasGroup imageCanvasGroup, bool toRestart, AudioSource audioSource)
     {
+        if (!_hasAudioPlayed)
+        {
+            audioSource.Play();
+            _hasAudioPlayed = true;
+        }
         _timer += Time.deltaTime;
 
         imageCanvasGroup.alpha = _timer / fadeDuration;
 
         if (toRestart)
         {
-            SceneManager.LoadScene(0);
+            StartCoroutine(ReloadLevel());
         }
         else
         {
             Debug.Log("Quitting");
             Application.Quit();
         }
+    }
+
+    IEnumerator ReloadLevel()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(0);
     }
 }
